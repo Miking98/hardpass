7bbe38c2f52a5217c511536ec8435bf29fa0573b
Lev Grossman <grossmanlev@users.noreply.github.com>
2018-12-05 17:03:49 -0500

    Adding debugging for install script

--- Windows/install_keyboard.py
+++ Windows/install_keyboard.py
@@ -13,19 +13,25 @@ if __name__ == "__main__":
     # Build AMD64 DLL
     rtn = subprocess.call(["bin\i386\kbdutool.exe", "-m", "{}".format(keyboard_file)])
     if rtn != 0:
+        print('Build failed!  Exiting...')
         sys.exit()
 
     # Move file
     rtn = subprocess.call("move {}.dll C:\Windows\System32".format(keyboard_name), shell=True)
     if rtn != 0:
+        print('Move failed! Try running in Administrator Mode...')
         sys.exit()
 
     # Build WOW64 DLL
     rtn = subprocess.call(["bin\i386\kbdutool.exe", "-o", "{}".format(keyboard_file)])
     if rtn != 0:
+        print('Build failed!  Exiting...')
         sys.exit()
 
     # Move file
     rtn = subprocess.call("move {}.dll C:\Windows\SysWOW64".format(keyboard_name), shell=True)
     if rtn != 0:
+        print('Move failed! Try running in Administrator Mode...')
         sys.exit()
+
+    # TODO: Add register file
